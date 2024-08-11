d9965dede2e5ef46351b50a194f8cab5e9492db3
Sans <saaniyanerekar@gmail.com>
2024-08-11 16:29:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      // Specify dependencies that should not be bundled
+      // For example, if you want to exclude `react` and `react-dom`:
+      external: ['react', 'react-dom'],
+      // Alternatively, if you really want to prevent a file from being bundled, use the `input` option
+      input: {
+        main: '/src/main.jsx'
+      }
+    }
+  }
+});
